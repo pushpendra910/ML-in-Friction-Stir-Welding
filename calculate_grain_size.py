0a82c2e5981daaaee8517588975d0c075ecf4e83
Pushpendra <pushpendrasingh.mec18@itbhu.ac.in>
2023-02-18 20:01:02 +0530

    new

--- calculate_grain_size.py
+++ calculate_grain_size.py
@@ -4,18 +4,10 @@ import os
 from skimage import morphology, measure
 import pandas as pd
 
-data=pd.DataFrame(columns=['Specimen_name','image_name','Grain_size'])
-conversion_factor=1
+data=pd.DataFrame(columns=['image_name','Grain_size_in_micron'])
+conversion_factor=1/12.2
 
-def calculate_grain_size(image_path,sub_filename):
-    if "100X" in sub_filename:
-        conversion_factor=1/1.22
-    elif "200X" in sub_filename:
-        conversion_factor=1/2.44
-    elif "500X" in sub_filename:
-        conversion_factor=1/6.1
-    elif "1000X" in sub_filename:
-        conversion_factor=1/12.2
+def calculate_grain_size(image_path):
     # Load the image in grayscale
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
@@ -47,7 +39,7 @@ def calculate_grain_size(image_path,sub_filename):
     return avg_grain_size*conversion_factor
 
 # specify the directory to iterate over
-dir_path = 'D:\Important\M.Teh thesis\For ML pushpendra\For ML pushpendra'
+dir_path = "D:\Important\M.Teh thesis\For ML pushpendra\Enlarged_images"
 
 # # loop through all files and directories in the specified directory
 for filename in os.listdir(dir_path):
@@ -58,28 +50,14 @@ for filename in os.listdir(dir_path):
     if os.path.isfile(file_path):
         # process the file
         print('Processing file:', file_path)
-        
-    # check if the current item is a directory
-    elif os.path.isdir(file_path):
-        # process the directory
-        print('Processing directory:', file_path)
-        for sub_filename in os.listdir(dir_path+"/"+filename):
-            # print(sub_filename,filename)
-            file_path=os.path.join(dir_path+"\\"+filename, sub_filename)
-            if os.path.isfile(file_path):
-                if file_path[-3:]!='xls' and file_path[-3:]!='csv':
-                    grain_size=calculate_grain_size(file_path,sub_filename)
-                
-                    sub_data=pd.DataFrame(columns=['Specimen_name','image_name','Grain_size'])
-                    sub_data['Specimen_name']=[filename]
-                    sub_data['image_name']=[sub_filename]
-                    sub_data['Grain_size']=[grain_size]
-                    # print(sub_data)
-                    data = pd.concat([data, sub_data], ignore_index=True)
-                    # data.append({image_name:grain_size},ignore_index=True)
-
-                    # print(file_path)
-            else:
-                print("Not going inside {file_path}")
+        grain_size=calculate_grain_size(file_path)
+    
+        sub_data=pd.DataFrame(columns=['image_name','Grain_size_in_micron'])
+        sub_data['image_name']=[filename]
+        sub_data['Grain_size_in_micron']=[grain_size]
+        # print(sub_data)
+        data = pd.concat([data, sub_data], ignore_index=True)
+        # data.append({image_name:grain_size},ignore_index=Tru
+        # print(file_path)
 print(data.head())
 data.to_csv("data.csv")
